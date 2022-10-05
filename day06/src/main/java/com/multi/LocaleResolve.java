28cafa577797fd3418d880da9b554a61b25a18b1
yyboom <yoonyoung37@naver.com>
2022-10-05 13:20:04 +0900

    20221005_lecture_day06

---
+++ day06/src/main/java/com/multi/LocaleResolve.java
@@ -0,0 +1,32 @@
+package com.multi;
+
+import java.util.Locale;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.SimpleLocaleContext;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.i18n.LocaleContextResolver;
+
+//어떤 브라우저에서 사용하고 있는지를 알려주는 것 언어의 팩을 바꿔주는 역할을 함
+@Configuration
+public class LocaleResolve implements LocaleContextResolver {
+
+    @Override
+    public LocaleContext resolveLocaleContext(ServerWebExchange exchange) {
+        //String language = exchange.getRequest().getHeaders().getFirst("Accept-Language");
+        String language = exchange.getRequest().getQueryParams().getFirst("lang");
+        Locale targetLocale = Locale.getDefault();
+        if (language != null && !language.isEmpty()) {
+            targetLocale = Locale.forLanguageTag(language);
+        }
+        return new SimpleLocaleContext(targetLocale);
+    }
+
+    @Override
+    public void setLocaleContext(ServerWebExchange exchange, LocaleContext localeContext) {
+        throw new UnsupportedOperationException("Not Supported");
+    }
+
+}
---
+++ day06/src/main/java/com/multi/WebConfig.java
@@ -0,0 +1,34 @@
+package com.multi;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+//configuration을 담당하는 클래스
+@Configuration
+public class WebConfig implements ApplicationContextAware {
+
+    ApplicationContext context;
+
+  
+    
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        //messages폴더 밑에 message를 불러온다.
+        messageSource.setBasenames("messages/message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) {
+        this.context = context;
+    }
+}
