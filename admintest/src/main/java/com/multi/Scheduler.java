e64c73a359f251adae32f5ac136ce90e393ac534
yyboom <yoonyoung37@naver.com>
2022-10-05 13:38:43 +0900

    20221005_lecture_admintest1

---
+++ admintest/src/main/java/com/multi/Scheduler.java
@@ -0,0 +1,27 @@
+package com.multi;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+	
+             //초 분 시 일 월 요일
+	//cron = "*  *  *  *  *  *"   
+	
+	//15초마다 스스로 동작함
+	@Scheduled(cron = "* * */15 * * *")
+	public void myScheduler1() {
+		System.out.println("Start My Scheduler1 ....");
+		System.out.println("End My Scheduler1 ....");
+	}
+	
+	//30초마다 스스로 동작함
+	@Scheduled(cron = "* * */30 * * *")
+	public void myScheduler2() {
+		System.out.println("Start My Scheduler2 ....");
+		System.out.println("End My Scheduler2 ....");
+	}
+
+
+}
